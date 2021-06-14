32f830eac1b080ec2b0551ac84aff7e77f94eacc
Pau Florentí Solano <pauf.97@gmail.com>
2021-06-14 13:33:21 +0200

    Editor layer class created

--- Sogas-Editor/src/EditorLayer.h
+++ Sogas-Editor/src/EditorLayer.h
@@ -1,30 +1,29 @@
-//#pragma once
-//
+#pragma once
+
 //#include "sogas.h"
-//
-//class Shader;
-//
-//class EditorLayer : public Layer
-//{
-//public:
-//	EditorLayer();
-//	virtual ~EditorLayer() = default;
-//
-//	virtual void onAttach() override;
-//	virtual void onDetach() override;
-//
-//	virtual void onUpdate(f32 dt) override;
-//	virtual void onImguiRender() override;
-//	virtual void onEvent(Event& event) override;
-//
-//private:
-//	Framebuffer* m_framebuffer;
-//	Camera* m_camera;
-//
-//	std::shared_ptr<VertexArray> m_vertexArray;
-//	std::shared_ptr<Texture2D> m_texture;
-//	std::shared_ptr<Shader> m_shader;
-//
-//	Mesh* mesh;
-//	glm::vec2 mouse_pos;
-//};
+#include "core/layer.h"
+
+class EditorLayer : public Layer
+{
+public:
+	EditorLayer();
+	virtual ~EditorLayer() = default;
+
+	virtual void onAttach() override;
+	virtual void onDetach() override;
+
+	virtual void onUpdate(f32 dt) override;
+	virtual void onImguiRender() override;
+	virtual void onEvent(Event& event) override;
+
+private:
+	/*Framebuffer* m_framebuffer;
+	Camera* m_camera;
+
+	std::shared_ptr<VertexArray> m_vertexArray;
+	std::shared_ptr<Texture2D> m_texture;
+	std::shared_ptr<Shader> m_shader;
+
+	Mesh* mesh;
+	glm::vec2 mouse_pos;*/
+};
