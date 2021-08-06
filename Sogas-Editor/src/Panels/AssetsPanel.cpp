caed89a62cc17373f02c3567d1e6248aeb4dd264
= <alvar.alonso.2010@gmail.com>
2021-08-06 17:17:05 +0200

    asset panel basic item layout

--- Sogas-Editor/src/Panels/AssetsPanel.cpp
+++ Sogas-Editor/src/Panels/AssetsPanel.cpp
@@ -1,5 +1,6 @@
 #include "AssetsPanel.h"
 #include "imgui.h"
+#include "defines.h"
 
 namespace Sogas {
 
@@ -22,26 +23,38 @@ namespace Sogas {
 			}
 		}
 
+		static f32 itemRightPadding = 16.0f;
+		static f32 thumbnailSize = 128.0f;
+		f32 cellSize = thumbnailSize + itemRightPadding;
+
+		f32 panelWidth = ImGui::GetContentRegionAvail().x;
+		i32 columnCount = (i32)(panelWidth / cellSize);
+		if (columnCount < 1)
+			columnCount = 1;
+
+		ImGui::Columns(columnCount, 0, false);
+
 		for(auto& directoryItem : std::filesystem::directory_iterator(m_currentDirectory))
 		{
 			const auto& path = directoryItem.path();
 			auto relativePath = std::filesystem::relative(path, s_assetsDirectory);
 			std::string relativeFilenameString = relativePath.filename().string();
 
-			if(directoryItem.is_directory())
+			ImGui::Button(relativeFilenameString.c_str(), {thumbnailSize, thumbnailSize});
+			if(ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left))
 			{
-				if(ImGui::Button(relativeFilenameString.c_str()))
-				{
+				if (directoryItem.is_directory())
 					m_currentDirectory /= path.filename();
-				}
 			}
-			else
-			{
-				if(ImGui::Button(relativeFilenameString.c_str()))
-				{
-				}
-			}
+			ImGui::Text(relativeFilenameString.c_str());
+
+			ImGui::NextColumn();
 		}
+
+		ImGui::Columns(1);
+
+		ImGui::SliderFloat("Thumbnail Size", &thumbnailSize, 16, 512);
+		ImGui::SliderFloat("Padding", &itemRightPadding, 0, 32);
 
 		ImGui::End();
 	}
